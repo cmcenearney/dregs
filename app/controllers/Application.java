f19c110cacbe9f703d08e38e3f17f9ca830b7556
Colin McEnearney <c.mcenearney@us.nature.com>
2013-12-10 22:09:16 -0500

    add title, metadata

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("DREGS!"));
+        return ok(index.render("DREGS! a Java regular expression editor"));
     }
 
     public static Result test() {
