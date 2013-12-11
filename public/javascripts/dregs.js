8a35bf2a0db81a760f2929dac7675c0d819df0d2
Colin McEnearney <c.mcenearney@us.nature.com>
2013-12-10 19:55:01 -0500

    redo Dregs.processInput

--- public/javascripts/dregs.js
+++ public/javascripts/dregs.js
@@ -8,7 +8,7 @@ Dregs = {
   alertColor: "#ff9898",
   regexInputElement: $("#regex"),
   resultsElement: $("#results"),
-  responseBuffer: "",  //use to capture repsonse objects for debugging
+
 
   //methods
   
@@ -27,6 +27,27 @@ Dregs = {
   resultsOk: function() {
     this.resultsElement.css( "background-color", this.inputBgColor );
   },
+
+  backslashesOK: function () {
+     if ($("#allowUnescaped").is(':checked')){
+        return true;
+     }
+      regexStr =  $("#regex").val();
+      var i=0;
+      while (i < regexStr.length) {
+          if (regexStr.charAt(i) == "\\") {
+              if (regexStr.charAt(i+1) == "\\") {
+                   i += 2;
+              } else {
+                    return false;
+              }
+          } else {
+              i++;
+          }
+      }
+      return true;
+  },
+
 
   getRegexStr: function() {
     regexStr =  $("#regex").val();
@@ -52,70 +73,69 @@ Dregs = {
     }
   },
 
-  handleSuccessfulResponse: function( data, textStatus, jqXHR ){
-    // debugging:
-    /*
-    console.log("successful request");
-    console.log(jqXHR);
-    this.responseBuffer = data;
-    */
-    if (data.msgIndex == this.lastMsgOut){
-        responseHtml = data.responseHtml;
-        this.resultsOk();
-        if (responseHtml == "" || responseHtml == null){
-            $("#results").html("<br/>");
-        } else {
-            $("#results").html(responseHtml);
-        }
-    } else {
-        console.log("too slow: " + data.msgIndex);
-    }
-
-  },
-
-  handleErringResponse: function( jqXHR, textStatus, errorThrown ){
-    // debugging:
-    /*
-    console.log("erring request");
-    console.log(jqXHR);
-    this.responseBuffer = jqXHR;
-    */
-    this.resultsAlert();
-    $("#results").html(jqXHR.responseJSON.responseHtml);
-  },
-
-  test: function() {console.log(this.getRegexStr())},
-
   processInput: function() {
-    _Dregs = this;
-    msg = new Object();
-    msg.searchStr = $("#test").val();
-    msg.regexStr = this.getRegexStr();
-    msg.msgIndex = ++this.lastMsgOut;
-    if (msg.searchStr.length==0 || msg.regexStr.length==0) {
-      $("#results").html("<br />");
-      return;
-    }
-    $.ajax({
-        url: "/dregs",
-        type: 'POST',
-        contentType: 'application/json',
-        data: JSON.stringify(msg),
-        dataType: 'json',
-        success: function( data, textStatus, jqXHR  ) {
-           _Dregs.handleSuccessfulResponse( data, textStatus, jqXHR );
-        },
-        error: function( jqXHR, textStatus, errorThrown ) {
-          _Dregs.handleErringResponse( jqXHR, textStatus, errorThrown );
+    if (!this.backslashesOK()) {
+        this.regexInputAlert();
+        return;
+    } else {
+        this.regexInputOk();
+        if ($("#regex").val().length==0 || $("#test").val().length==0) {
+            this.resultsOk();
+            $("#results").html("<br />");
+            return;
+        } else {
+            this.sendToServer();
+            return;
         }
-    });
-  }
+    }
+  },
+
+  sendToServer: function() {
+      _Dregs = this;
+      msg = new Object();
+      msg.searchStr = $("#test").val();
+      msg.regexStr = this.getRegexStr();
+      msg.msgIndex = ++this.lastMsgOut;
+      $.ajax({
+          url: "/dregs",
+          type: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify(msg),
+          dataType: 'json',
+          success: function( data, textStatus, jqXHR  ) {
+             _Dregs.handleSuccessfulResponse( data, textStatus, jqXHR );
+          },
+          error: function( jqXHR, textStatus, errorThrown ) {
+            _Dregs.handleErringResponse( jqXHR, textStatus, errorThrown );
+          }
+      });
+  },
+
+    handleSuccessfulResponse: function( data, textStatus, jqXHR ){
+      if (data.msgIndex == this.lastMsgOut){
+          responseHtml = data.responseHtml;
+          this.resultsOk();
+          if (responseHtml == "" || responseHtml == null){
+              $("#results").html("<br/>");
+          } else {
+              $("#results").html(responseHtml);
+          }
+      } else {
+          console.log("too slow: " + data.msgIndex);
+      }
+
+    },
+
+    handleErringResponse: function( jqXHR, textStatus, errorThrown ){
+      this.resultsAlert();
+      $("#results").html(jqXHR.responseJSON.responseHtml);
+    }
 
 };
 
 
 /*
-wire up the Dregs object to some dom listeners
+wire up the Dregs object to some DOM listeners
 */
 $( document ).ready(function() {
   
